b280a8fac980522b354f95cfc8e3d5fcf3f9841f
Chunt0 <chunt.mail@gmail.com>
2026-06-19 21:11:29 +0000

    Moving stuff around

--- core/app_env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def app_env(name: str, default=None):
-    """Read PUTTYU_<name>. `name` is the suffix WITHOUT the prefix, e.g.
-    app_env("MEMORY_IMPORT_MAX_BYTES", "...")."""
-    return os.environ.get(f"PUTTYU_{name}", default)
